12724abccb66f67cad6134fb9812e1fad90d6941
tayyab900 <86831891+tayyab900@users.noreply.github.com>
2021-07-02 04:05:17 -0700

    Add files via upload

---
+++ BIIT_FB_WebService/BIIT_FB_WebService/DB_Handler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace BIIT_FB_WebService
+{
+    public class DB_Handler
+    {
+        SqlConnection sqlcon;
+        SqlCommand sqlcom;
+        SqlDataAdapter adp;
+
+
+        public DB_Handler()
+        {
+            sqlcon = new SqlConnection(@"Data Source=PROBOOK;Initial Catalog=BIIT_FB;Persist Security Info=True;User ID=sa;Password=123");
+            sqlcom = new SqlCommand();
+
+        }
+        private bool openConnection()
+        {
+            try
+            {
+                sqlcon.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        private void closeConnection()
+        {
+            sqlcon.Close();
+        }
+        public object ExecuteAggResult(string query)
+        {
+            if (openConnection())
+            {
+                openConnection();
+                sqlcom.CommandText = query;
+                sqlcom.Connection = sqlcon;
+                object result = sqlcom.ExecuteScalar();
+                closeConnection();
+                return result;
+            }
+            else
+                return null;
+        }
+
+        public DataTable ExecuteSelect(string query)
+        {
+            // if (openConnection())
+            {
+                openConnection();
+                var dt = new DataTable();
+                adp = new SqlDataAdapter(query, sqlcon);
+                adp.Fill(dt);
+                closeConnection();
+                int b = dt.Rows.Count;
+                return dt;
+            }
+            //return null;
+        }
+
+        public SqlDataReader ExecuteSelect_reader(string query)
+        {
+            // if (openConnection())
+            {
+                openConnection();
+                sqlcom = new SqlCommand(query, sqlcon);
+                SqlDataReader sdr;
+                sdr = sqlcom.ExecuteReader();
+                closeConnection();
+                return sdr;
+            }
+            //return null;
+        }
+
+        public bool ExecuteQuery(string query)
+        {
+            try
+            {
+
+                openConnection();
+                sqlcom.CommandText = query;
+                sqlcom.Connection = sqlcon;
+                sqlcom.ExecuteNonQuery();
+                closeConnection();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+
+}
---
+++ BIIT_FB_WebService/BIIT_FB_WebService/IService1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BIIT_FB_WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/Login/{regno}/{password}")]
+        string Login(String regno, String password);//"/Login?regno={regno}&password={password}"
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/GetNumber/{regno}")]
+        string GetNumber(String regno);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/sendsms/{regno}")]
+        String sendsms(String regno);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/varifypin/{regno}/{pin}")]
+        String varify_pin(String regno, String pin);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/setpassword/{regno}/{pass}")]
+        String setpassword(String regno, String pass);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/Get_Acc_info/{Accno}")]
+        List<Std_info> Get_Acc_info(String Accno);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Set_profile_images")]
+        String Set_profile_images(String Accno,String Save_as,String imgbase64);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/Get_Cl_fr/{regno}")]
+        List<Std_info> Get_Class_friend(String regno);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "/Get_bat_fr/{regno}")]
+        List<Std_info> Get_Batch_friend(String regno);
+
+       
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
